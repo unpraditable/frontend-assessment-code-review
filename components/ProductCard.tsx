200f51b8c672ae6bfe11d68f95e05c717ce29649
Aditya Pradita <pradita397@gmail.com>
2026-07-28 23:08:19 +0700

    Fix: change product image from using <img> to <Image>

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import { Product } from "@/interfaces/IProduct";
+import Image from "next/image";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
     <div className="card" onClick={() => alert(`Added ${product.name}`)}>
-      <img src={product.image} alt={product.name} width={200} height={200} />
+      <Image src={product.image} alt={product.name} width={200} height={200} />
       <h2>{product.name}</h2>
       <p>{product.description}</p>
       <p>${product.price}</p>

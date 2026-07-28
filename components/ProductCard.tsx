cd6dbccc6ff6e722b9c46e1514510d2575ba5ac3
Aditya Pradita <pradita397@gmail.com>
2026-07-28 23:06:00 +0700

    Feat: add alt attribute to product image

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -3,7 +3,7 @@ import { Product } from "@/interfaces/IProduct";
 export function ProductCard({ product }: { product: Product }) {
   return (
     <div className="card" onClick={() => alert(`Added ${product.name}`)}>
-      <img src={product.image} width={200} height={200} />
+      <img src={product.image} alt={product.name} width={200} height={200} />
       <h2>{product.name}</h2>
       <p>{product.description}</p>
       <p>${product.price}</p>
